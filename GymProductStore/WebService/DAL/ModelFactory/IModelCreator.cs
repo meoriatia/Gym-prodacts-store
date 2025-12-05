0edadebfff3309bbf8f66b57dc73af86f89a4be7
Meori Atia <meoriatia@gmail.com>
2025-12-05 12:47:20 +0200

    GymProductStore(): Remove Duplicate

--- GymProductStore/WebService/DAL/ModelFactory/IModelCreator.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Data;  // מייבא עבודה עם נתונים (IDataReader)
-
-namespace WebService  // מגדיר מרחב שם
-{
-    public interface IModelCreator<T>  // ממשק שמגדיר פונקציה ליצירת מודל מסוג T
-    {
-        T CreateModel(IDataReader src);  // פונקציה שיוצרת אובייקט מסוג T מתוך IDataReader
-    }
-}
